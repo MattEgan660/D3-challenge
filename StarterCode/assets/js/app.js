13c8c974ef93a5ff0320082dfd8e86c7fe2e534d
MattEgan660 <61856548+MattEgan660@users.noreply.github.com>
2021-03-01 13:39:30 -0500

    added svg and chartgroup variables

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -10,3 +10,24 @@ var margin = {
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+
+
+
+d3.csv("assets/data/data.csv").then(function(demographicData) {
+    demographicData.forEach(function(data){
+        data.healthcare = 
+        data.poverty = +data.poverty
+    });
+
+});
